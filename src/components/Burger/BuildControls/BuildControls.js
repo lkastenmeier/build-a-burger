2594c82c77045e3b1bb7af94df1407b188f210c5
lkastenmeier <l.kastenmeier@web.de>
2020-01-27 15:40:48 +0100

    render controls with styling

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./BuildControls.module.css";
+import BuildContol from "./BuildControl/BuildControl";
+const controls = [
+  { label: "Salad", type: "salad" },
+  { label: "Cheese", type: "cheese" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Meat", type: "meat" }
+];
+
+const BuildControls = props => {
+  return (
+    <div className={style.BuildControls}>
+      {controls.map(control => (
+        <BuildContol key={control.label} label={control.label} />
+      ))}
+    </div>
+  );
+};
+export default BuildControls;

b8827afa084fdeaa35e45426617ac5def4e0b94c
lkastenmeier <l.kastenmeier@web.de>
2020-01-27 15:16:44 +0100

    display message if no ingredients were added yet

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,18 @@ import React from "react";
 import Burgeringredient from "./Burgeringredient/Burgeringredient";
 import style from "./Burger.module.css";
 const Burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    ingredient => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredient => {
       return [...Array(props.ingredients[ingredient])].map((_, i) => {
         return <Burgeringredient key={ingredient + i} type={ingredient} />;
       });
-    }
-  );
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Start adding ingredients!</p>;
+  }
   return (
     <div className={style.Burger}>
       <Burgeringredient type="bread-top" />

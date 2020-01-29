92583a326ba8e22a2d3961dc5639c226e0d4771a
lkastenmeier <l.kastenmeier@web.de>
2020-01-29 16:11:25 +0100

    add error handler

--- src/containers/BurgerBuilder.js/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-orders";
 import { Spinner } from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -22,7 +23,6 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
-    purchasing: false,
     loading: false,
     purchasing: false
   };
@@ -137,4 +137,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
